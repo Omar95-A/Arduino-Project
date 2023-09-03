7b305e042df34c1b262ce400cf55708a25ef665f
omarak <omerakrum02@gmail.com>
2023-09-03 20:27:19 +0300

    upload project

---
+++ sketch_may15d/sketch_may15d.ino
@@ -0,0 +1,18 @@
+
+#include <Keypad.h>
+#include<Servo.h>
+#define ledr 6
+#define ledb 7
+#define trig 8
+#define echo 9
+#define buzzer 12
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
