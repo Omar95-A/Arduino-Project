4964c97ed09edb0972b3a0af11338bc91b9a2ca7
omarak <omerakrum02@gmail.com>
2023-09-12 15:59:16 +0300

    Adding Array for keypad

--- sketch_may15d/sketch_may15d.ino
+++ sketch_may15d/sketch_may15d.ino
@@ -10,6 +10,12 @@
 int kere=0;
 const byte ROWS = 4; 
 const byte COLS = 4; 
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
 void setup() {
   // put your setup code here, to run once:
 
