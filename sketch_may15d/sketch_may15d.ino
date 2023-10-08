d1576e32c6c913570918668c8f517af7d40a24b6
omarak <omerakrum02@gmail.com>
2023-10-08 11:51:18 +0300

    update c++ code

--- sketch_may15d/sketch_may15d.ino
+++ sketch_may15d/sketch_may15d.ino
@@ -86,6 +86,16 @@ void loop() {
         i=0;
         count=0;
         }
-      }
+      }else {
+        Serial.println("\nŞifre Yanlış, lutfen tekrar deneyin ");
+        digitalWrite(ledr,HIGH);
+        digitalWrite(buzzer,HIGH);
+        delay(1000);
+        digitalWrite(ledr,LOW);
+        digitalWrite(buzzer,LOW);
+        i=0;
+        count=0;
+          
+        }
     }
 }
